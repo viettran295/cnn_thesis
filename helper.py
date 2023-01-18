9091cdb01cf25641c0db6d76b3a9212ba9b8696a
viet.tran <viettran295@gmail.com>
2023-01-18 10:37:40 +0100

    ingest data to multivar reg

--- helper.py
+++ helper.py
@@ -127,16 +127,11 @@ def img_preprocess_pipeline(img_path_arr, steering_arr, train_flag=True):
 # create general model
 def build_network(activation, optimizer, dropout):
     model = Sequential()
-    model.add(Conv2D(24, (5,5), (1,1), input_shape=(70, 200, 3), activation=activation)) # (filter, kernel, stride, input shape)
-    model.add(Dropout(dropout))
-    model.add(Conv2D(36, (5,5), (1,1), activation=activation)) 
-    model.add(Dropout(dropout))
-    model.add(Conv2D(48, (5,5), (2,2), activation=activation)) 
-    model.add(Dropout(dropout))
-    model.add(Conv2D(64, (3,3), (2,2), activation=activation)) # size of img small -> stride = 1
-    model.add(Dropout(dropout))
-    model.add(Conv2D(64, (3,3), (2,2), activation=activation)) 
-    model.add(Dropout(dropout))
+    model.add(Conv2D(24, (5,5), (2,2), input_shape=(70, 200, 3), activation='elu')) # (filter, kernel, stride, input shape)
+    model.add(Conv2D(36, (5,5), (2,2), activation='elu')) 
+    model.add(Conv2D(48, (5,5), (2,2), activation='elu')) 
+    model.add(Conv2D(64, (3,3), activation='elu')) # size of img small -> stride = 1
+    model.add(Conv2D(64, (3,3), activation='elu')) 
 
     model.add(Flatten())
     model.add(Dense(100, activation=activation))
@@ -145,7 +140,7 @@ def build_network(activation, optimizer, dropout):
     model.add(Dropout(dropout))
     model.add(Dense(10, activation=activation))
     model.add(Dropout(dropout))
-    model.add(Dense(1, activation='tanh'))
+    model.add(Dense(1))
 
     model.compile(loss='mse', optimizer=optimizer, metrics=['acc'])
 
--- wandb_config.py
+++ wandb_config.py
@@ -19,28 +19,28 @@ sweep_config = {
             'values': [10]
         },
         'batch_size': {
-            'distribution': 'int_uniform',
-            'min': 120,
-            'max': 240
-            # 'values': [10, 20, 40, 60, 80, 100, 120]
+            # 'distribution': 'int_uniform',
+            # 'min': 10,
+            # 'max': 120
+            'values': [10, 20, 40, 60, 80, 100, 120, 140]
         },
         'dropout': {
-            'distribution': 'uniform',
-            'min': 0.1,
-            'max': 0.7
-            # 'values': [0.2, 0.4, 0.6, 0.8]
+            # 'distribution': 'uniform',
+            # 'min': 0.1,
+            # 'max': 0.7
+            'values': [0, 0.2, 0.4]
         },
         'learning_rate': {
-            'distribution': 'uniform',
-            'min': 0.00001, 
-            'max': 0.001
-            # 'values': [0.001, 0.0025, 0.005, 0.0075, 0.01]
+            # 'distribution': 'uniform',
+            # 'min': 0.0001, 
+            # 'max': 0.01
+            'values': [0.0001, 0.00025, 0.0005, 0.00075, 0.001, 0.0025, 0.005, 0.0075, 0.01]
         },
         'optimizer':{ 
             'values': ['Adam']
         },
         'activation': {
-            'values': ['tanh']
+            'values': ['tanh', 'elu']
         }
     }
 }
